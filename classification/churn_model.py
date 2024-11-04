77244a5d3a9310f903941964db42c574ef4c779c
Olaoluwa Tunmise <oluwatunmise1olaoluwa@gmail.com>
2024-11-04 14:56:59 +0100

    churn_model

--- classification/churn_model.py
+++ classification/churn_model.py
@@ -1,25 +1,20 @@
-#!/usr/bin/env python
-# coding: utf-8
 
 import pickle
 import pandas as pd
 import numpy as np
- 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
- 
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score
 
-# parameters
-
+# Setting model Parameters
 C = 1.0
 n_splits = 5
+
 output_file = f'model_C={C}.bin'
 
-# Data preparation
- 
+# Data preparation 
 df = pd.read_csv('/Users/olaoluwatunmise/ML_Zoomcamp/classification/Telcos_Cus_Churn.csv')
  
 df.columns = df.columns.str.lower().str.replace(' ', '_')
@@ -34,14 +29,9 @@ df.totalcharges = df.totalcharges.fillna(0)
  
 df.churn = (df.churn == 'yes').astype(int)
 
-
-
-
 # Data splitting
  
 df_full_train, df_test = train_test_split(df, test_size=0.2, random_state=1)
-
-
 
 numerical = ['tenure', 'monthlycharges', 'totalcharges']
  
@@ -52,7 +42,7 @@ categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
        'paymentmethod']
 
 
-#training
+# Training
 
 
 def train(df_train, y_train, C=1.0):
@@ -79,7 +69,7 @@ def predict(df, dv, model):
 
 
 
-# validation
+# Validation
 
 print(f'doing validation with C={C}')
 
